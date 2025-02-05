89d24102c45dbec683d1b47449a65cc49d56bc5b
rafitis <rafitis00@gmail.com>
2025-02-05 21:51:55 +0100

    Delete vercel

--- .vercel/output/_functions/chunks/supabase_BmmuNb-i.mjs
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  "https://rsimzjxfezlmbsjfngfn.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzaW16anhmZXpsbWJzamZuZ2ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzg1OTc3MTksImV4cCI6MjA1NDE3MzcxOX0.1T72aUTJej9hMb9IyAH5Ernh3mnMeJqKSqPi0c6-N9U"
-);
-
-export { supabase as s };
