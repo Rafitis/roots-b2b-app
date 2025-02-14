81039705e2661a6460c628d2d1b1f7e3f2b68aea
rafitis <rafitis00@gmail.com>
2025-02-14 19:45:56 +0100

    Se arreglan estilos de componente

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -26,9 +26,9 @@
   
       return (
         <>
-        <div class="flex flex-row gap-2 pb-4">
+        <div className="flex flex-row gap-2 pb-4">
           {product?.Colores && (
-        <select defaultValue="Color" key="color" class="select select-bordered w-full max-w-xs" onChange={(e) => setColorSelected(e.target.value)}>
+        <select defaultValue="Color" key="color" className="select select-bordered w-full max-w-xs" onChange={(e) => setColorSelected(e.target.value)}>
             <option disabled>Color</option>
             {product?.Colores.map((color) => (
               <option key={color} value={color}>{color}</option>
@@ -36,7 +36,7 @@
         </select>)
         }
         {product?.Talla && (
-          <select defaultValue="Talla" key="size" class="select select-bordered w-full max-w-xs" onChange={(e) => setSizeSelected(e.target.value)}>
+          <select defaultValue="Talla" key="size" className="select select-bordered w-full max-w-xs" onChange={(e) => setSizeSelected(e.target.value)}>
             <option disabled>Talla</option>
             {product?.Talla.map((size) => (
               <option key={size} value={size}>{size}</option>
@@ -44,10 +44,10 @@
           </select>
         )}
     </div>
-    <div class="pb-2">
-        <input id={"cantidad-producto" + product?.id} type="text" placeholder="Cantidad" class="input input-bordered w-full max-w-xs" onChange={(e) => setQuantity(Number(e.target.value))} />
+    <div className="pb-2">
+        <input id={"cantidad-producto" + product?.id} type="text" placeholder="Cantidad" className="input input-bordered w-full max-w-xs" onChange={(e) => setQuantity(Number(e.target.value))} />
     </div>
-    <div class="card-actions justify-center pb-4">
+    <div className="card-actions justify-center pb-4">
       <button className="btn btn-primary btn-md" onClick={handleAddElementToCart}>
         <Toaster position="top-right" reverseOrder={false} />
         <CartIcon />
