7b16860c5e9c5da5a1a5228c9c53692f39e4ee63
rafitis <rafitis00@gmail.com>
2025-04-21 20:28:23 +0200

    Add @midudev/tailwind-animations dependency and integrate into Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
+
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    plugins: [require("daisyui")],
+    plugins: [require("daisyui"), animations],
     daisyui: {
       themes: ["cupcake"],
     },
